495054f0e9f755ebb8f998c4935cb00ddb8cf66d
birdroid <aineinefert@yandex.ru>
2016-09-25 22:43:50 +0500

    I actually did this ???

--- BARWP/js/javascript.js
+++ BARWP/js/javascript.js
@@ -1,4 +1,15 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-  $( function() {
-    $( "#accordion div" ).accordion();
-  } );
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+  }
+}
+
+$(document).ready(function() {
+    $(".accordion").click(function() {
+      console.log("clicked: %o", this);
+    });
+  });
